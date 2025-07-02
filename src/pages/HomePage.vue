411ef21f9ef18b58e08c0a26ddd990573960a2f8
程嘉仑 <2060140789@qq.com>
2025-07-02 16:36:06 +0800

    图片主页完善

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,33 @@
 <template>
   <div id="homePage">
+    <!-- 搜索框 -->
+    <div class="search-bar">
+      <a-input-search
+        placeholder="从海量图片中搜索"
+        v-model:value="searchParams.searchText"
+        enter-button="搜索"
+        size="large"
+        @search="doSearch"
+      />
+    </div>
+    <!-- 分类 + 标签 -->
+    <a-tabs v-model:activeKey="selectedCategory" @change="doSearch">
+      <a-tab-pane key="all" tab="全部" />
+      <a-tab-pane v-for="category in categoryList" :key="category" :tab="category" />
+    </a-tabs>
+    <div class="tag-bar">
+      <span style="margin-right: 8px">标签：</span>
+      <a-space :size="[0, 8]" wrap>
+        <a-checkable-tag
+          v-for="(tag, index) in tagList"
+          :key="tag"
+          v-model:checked="selectedTagList[index]"
+          @change="doSearch"
+        >
+          {{ tag }}
+        </a-checkable-tag>
+      </a-space>
+    </div>
     <!-- 图片列表 -->
     <a-list
       :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
@@ -8,8 +36,30 @@
       :loading="loading"
     >
       <template #renderItem="{ item: picture }">
-        <a-list-item>
+        <a-list-item style="padding: 0">
           <!-- 单张图片 -->
+          <!-- 单张图片 -->
+          <a-card hoverable @click="doClickPicture(picture)">
+          <template #cover>
+              <img
+                style="height: 180px; object-fit: cover"
+                :alt="picture.name"
+                :src="picture.url"
+              />
+            </template>
+            <a-card-meta :title="picture.name">
+              <template #description>
+                <a-flex>
+                  <a-tag color="green">
+                    {{ picture.category ?? '默认' }}
+                  </a-tag>
+                  <a-tag v-for="tag in picture.tags" :key="tag">
+                    {{ tag }}
+                  </a-tag>
+                </a-flex>
+              </template>
+            </a-card-meta>
+          </a-card>
         </a-list-item>
       </template>
     </a-list>
@@ -20,8 +70,9 @@
 <script setup lang="ts">
 // 数据
 import { computed, onMounted, reactive, ref } from 'vue'
-import { listPictureVoByPageUsingPost } from '@/api/pictureController.ts'
+import { listPictureTagCategoryUsingGet, listPictureVoByPageUsingPost } from '@/api/pictureController.ts'
 import { message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
 
 const dataList = ref<API.PictureVO[]>([])
 const total = ref(0)
@@ -42,7 +93,7 @@ const pagination = computed(() => {
     pageSize: searchParams.pageSize ?? 10,
     total: total.value,
     // 切换页号时，会修改搜索参数并获取数据
-    onChange: (page, pageSize) => {
+    onChange: (page:number, pageSize:number) => {
       searchParams.current = page
       searchParams.pageSize = pageSize
       fetchData()
@@ -53,7 +104,20 @@ const pagination = computed(() => {
 // 获取数据
 const fetchData = async () => {
   loading.value = true
-  const res = await listPictureVoByPageUsingPost(searchParams)
+  // 转换搜索参数
+  const params = {
+    ...searchParams,
+    tags: [] as string[],
+  }
+  if (selectedCategory.value !== 'all') {
+    params.category = selectedCategory.value
+  }
+  selectedTagList.value.forEach((useTag, index) => {
+    if (useTag) {
+      params.tags.push(tagList.value[index])
+    }
+  })
+  const res = await listPictureVoByPageUsingPost(params)
   if (res.data.data) {
     dataList.value = res.data.data.records ?? []
     total.value = res.data.data.total ?? 0
@@ -63,14 +127,63 @@ const fetchData = async () => {
   loading.value = false
 }
 
+
 // 页面加载时请求一次
 onMounted(() => {
   fetchData()
 })
 
+// 搜索
+const doSearch = () => {
+  // 重置搜索条件
+  searchParams.current = 1
+  fetchData()
+}
+
+
+const categoryList = ref<string[]>([])
+const selectedCategory = ref<string>('all')
+const tagList = ref<string[]>([])
+const selectedTagList = ref<boolean[]>([])
+
+// 获取标签和分类选项
+const getTagCategoryOptions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    // 转换成下拉选项组件接受的格式
+    categoryList.value = res.data.data.categoryList ?? []
+    tagList.value = res.data.data.tagList ?? []
+  } else {
+    message.error('加载分类标签失败，' + res.data.message)
+  }
+}
+
+onMounted(() => {
+  getTagCategoryOptions()
+})
+
+const router = useRouter()
+// 跳转至图片详情
+const doClickPicture = (picture: API.PictureVO) => {
+  router.push({
+    path: `/picture/${picture.id}`,
+  })
+}
+
+
+
 </script>
 
 <style scoped>
 #homePage {
+  margin-bottom: 16px;
 }
+#homePage .search-bar {
+  max-width: 480px;
+  margin: 0 auto 16px;
+}
+#homePage .tag-bar {
+  margin-bottom: 16px;
+}
+
 </style>
